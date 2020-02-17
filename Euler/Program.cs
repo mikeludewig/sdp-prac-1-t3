394f541b4e47c9b920f0b2875a8c2f17eb1fdeb4
Ana <anarita.dauane@gmail.com>
2020-02-17 23:24:48 +0200

    Solution Added 2

--- Euler/Program.cs
+++ Euler/Program.cs
@@ -7,6 +7,34 @@ namespace Euler
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-        }
+            // Solution for Problem 3, g18d5811
+            long n = 600851475143; // inicilalizing the variable int64 bit given 
+            long num = 0; // also the factor to be equal to 0
+
+            for (long i = 2; i < n; i++) // the for loop to throught the number starting with the prime number 2 
+            {
+                if (n % i == 0) // if the number is divisble by itself 
+                {
+
+                    bool primeNum = true; // the number is prime 
+
+                    for (long j = 2; j < i; j++) // is goes again in the loop to find the largest number 
+                    {
+                        if (i % j == 0) // if the numbers are the same 
+                        {
+                            primeNum = false; // is not a prime number 
+                            break; // leaves the loop 
+                        }
+                    }
+                    if (primeNum) // if the number is prime 
+                    {
+                        num = i; // the number is the largest prime number 
+
+                    }
+                    Console.WriteLine(num); // prints the number 
+                    Console.ReadLine();// breaks a line 
+
+                }
+            }
     }
 }
